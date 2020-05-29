9308eddce9564f633a56f0252df16241ca4afb82
xeilen <xeilen1112@gmail.com>
2020-05-29 16:50:41 +0300

    1 component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+    state = {
+        currentValue: 0,
+        inputValue : ''
+
+    }
+    addOneClick = () => {
+        this.setState({
+            currentValue: this.state.currentValue + 1
+        })
+    }
+
+    minusOneCkick = () => {
+        if (this.state.currentValue <= 0) {
+            return
+        }
+        this.setState({
+            currentValue: this.state.currentValue - 1
+        })
+    }
+
+    plus100Click = () => {
+        this.setState({
+            currentValue: this.state.currentValue + 100
+        })
+    }
+    minus100Click = () => {
+        if (this.state.currentValue <= 100) {
+            return this.setState({
+                currentValue: 0
+            })
+        }
+        this.setState({
+            currentValue: this.state.currentValue - 100
+        })
+    }
+
+     inputOnChange =(event) => {
+
+            this.setState({
+                inputValue: event.target.value
+            })
+    }
+
+    addFromInput = () => {
+        this.setState({
+            currentValue: this.state.currentValue + Number(this.state.inputValue),
+
+
+        })
+
+
+    }
+
+    render() {
+
+        // console.log(this.state.inputValue)
+        return (
+            <div className="App">
+                <div className={`result`}>
+                    {this.state.currentValue}
+                </div>
+                <div className={`buttons`}>
+                    <div className={`counter`}>
+                        <button onClick={this.addOneClick}>+1</button>
+                        <button onClick={this.minusOneCkick}>-1</button>
+                        <button onClick={this.plus100Click}>+100</button>
+                        <button onClick={this.minus100Click}>-100</button>
+                    </div>
+                    <div className={`input`}>
+                        <input type="text" onChange={this.inputOnChange}/>
+                        <button onClick={this.addFromInput}>add</button>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+
+
 }
 
-export default App;
+
