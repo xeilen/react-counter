c9ab3d770f81a0df7ada8298be5fbf9cc8f75e89
xeilen <xeilen1112@gmail.com>
2020-05-31 20:10:26 +0300

    --1

---
+++ src/resultComponent/Result.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import './styleResult.css'
+
+export class Result extends Component{
+constructor(props) {
+    super(props);
+}
+
+    render() {
+    const {val, count} = this.props
+        return (
+            <div className={`result`}>
+                <div>{val}</div>
+            </div>
+        )
+    }
+}
